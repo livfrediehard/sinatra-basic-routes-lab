fe2dd3b028b2fb8896c094c5f25c66f42b961605
livfrediehard <63824731+livfrediehard@users.noreply.github.com>
2021-10-17 13:43:46 -0400

    Done.

--- app.rb
+++ app.rb
@@ -2,3 +2,20 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 end
+
+require_relative 'config/environment'
+
+class App < Sinatra::Base
+end
+
+get '/name' do
+    "My name is Brian."
+end 
+
+get '/hometown' do
+    "My hometown is Cleveland, TN."
+end
+
+get '/favorite-song' do 
+    "My favorite song is Jimmy Olsen's Blues."
+end 
