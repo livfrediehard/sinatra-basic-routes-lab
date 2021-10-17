c2cf20f3977c4922ef326d48c4ec0240cd94dc69
Brian <bpalmer710@gmail.com>
2021-10-17 13:44:05 -0400

    commit tree a68c69f5ba0e34fe6ae4a12ae311148af9a49016

--- app.rb
+++ app.rb
@@ -2,3 +2,15 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 end
+
+get '/name' do
+    "My name is Brian."
+end 
+
+get '/hometown' do
+    "My hometown is Cleveland, TN."
+end
+
+get '/favorite-song' do 
+    "My favorite song is Jimmy Olsen's Blues."
+end 
